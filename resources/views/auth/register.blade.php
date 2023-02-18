3cde9ca7a5866d02f1b51bac3485222363f58c69
SajjadKhalafi <sajjad01khalafi@protonmail.com>
2023-02-18 13:19:31 +0330

    login with username

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- username Address -->
+        <!-- username -->
         <div class="mt-4">
             <x-input-label for="username" :value="__('Username')" />
             <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autocomplete="username" />
